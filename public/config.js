e43d242a95162621345731960534d8456d36df54
Mario Reder <mario.reder@pm.me>
2025-11-23 09:56:58 +0100

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -19,7 +19,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "true",
   "VITE_ENABLED_MENUS": "Trading,Swap,Portfolio,Markets,Leaderboard,Rewards,Vaults",
-  "VITE_CUSTOM_MENUS": "The $GANG token, https://revshare.dev/token/FguCo5yimhUw7tTGpPZYcvB98mjfVVMGn5dC2m3SYFxx",
+  "VITE_CUSTOM_MENUS": "The $GANG token, https://join.pump.fun/HSag/kw5mhxc5",
   "VITE_ENABLE_SERVICE_DISCLAIMER_DIALOG": "false",
   "VITE_ENABLE_CAMPAIGNS": "false",
   "VITE_TELEGRAM_URL": "https://t.me/thegangonsolana",
